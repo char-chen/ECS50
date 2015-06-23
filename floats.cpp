eb528f7dec890827132e498ccfe763dff93f0966
Charles Chen <cyuchen@ucdavis.edu>
2015-06-23 12:13:51 -0700

    first stage

--- floats.cpp
+++ floats.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+void printHex(char *i, char *f);
 
 int main()
 {
-
+  char input[256];
+  cout << "Number: ";
+  cin >> input;
+  char *tok = strtok(input , ".");
+  char integer[strlen(tok) + 1];
+  strcpy(integer, tok);
+  tok = strtok(NULL, "");
+  char fraction[strlen(tok) + 1];
+  strcpy(fraction, tok);
+  printHex(integer, fraction);  
 } //main
+
+void printHex(char *in, char *frac)
+{
+  
+}
